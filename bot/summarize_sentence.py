f86967ea90c423488a7fa72eb710f00f633e0e94
harshavardan423 <harshavardan423@gmail.com>
2023-02-19 23:19:38 +0530

    commit

--- bot/summarize_sentence.py
+++ bot/summarize_sentence.py
@@ -3,7 +3,7 @@ from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize, sent_tokenize
 from nltk.stem import WordNetLemmatizer
 
-def summarize_text(text, num_sentences=3):
+def summarize_text(text, num_sentences=10):
     # Tokenize the text into sentences and words
     sentences = sent_tokenize(text)
     words = word_tokenize(text.lower())
@@ -38,6 +38,22 @@ def summarize_text(text, num_sentences=3):
 
 
 text = """
+
+Washington Capitals forward Garnet Hathaway kept a blog throughout the 2023 Navy Federal Credit Union NHL Stadium Series. The Capitals lost to the Carolina Hurricanes 4-1 at Carter-Finley Stadium in Raleigh, North Carolina, on Saturday. In his final entry, Hathaway discusses what went wrong for Washington, the experience of his first NHL outdoor game and how the team needs to move on quickly.
+
+We talked about it as a team beforehand, how a win tonight would have made this game a great memory. To lose it, it stings. 
+
+It stings more than ever because those were two points that we really needed right now; I think that's what guys will kind of dwell on, but we can't do anything else tonight about it. When you lose a game, you want to figure out immediately, what you could've done to win it and there are, unfortunately, a lot of things right now. 
+
+It was one of those things where we wish we could have it back. It would make this moment a lot sweeter, but unfortunately, we can't. For tonight, we have to continue to enjoy this moment.
+
+Everything leading up to the game was awesome. Those fans were rowdy. Even playing two-touch soccer in front of the fans -- a lot of Capitals fans traveled too -- and it was such a cool environment that I felt really fortunate to play in. That walkout before the game, it was special, along with the flyover, the fireworks, and having 56, 57,000 people in the stadium. And when the lights turned off and the cell phones turned on, that's a moment I'll never forget.
+
+Giving up that first goal 2:11 into the game, that's not a recipe for success. More times than not, you're going to lose when you do that and you're trying to get out of that hole. And the earlier it happens in a game, the harder it is to get out of it and, unfortunately, they got the next one and they got the next one and they got the next one.
+
+We have a few things that we look over at the end of the game and say, "Did we do this? Did it help us succeed?" I thought we put a lot of pressure on them in the first period and that goal hurt on the scoreboard, but I don't think it slowed us down mentally. We've been down one goal before. We've been down two and we've come back. There was never a moment in the game when we thought we couldn't come back from this.
+
+That's the belief on this team and that's the leadership in this locker room. In order to come back, we've got to do a lot more things right.
 
 """
 summary = summarize_text(text, num_sentences=2)
